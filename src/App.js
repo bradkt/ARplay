f6656a7d0e6b150851602be46e5526ee250129e6
Brad Tracy <btracy@everesttech.com>
2019-08-14 15:44:46 -0400

    closing tag

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import './App.css';
 import * as seahorse from './assets/seahorse.gltf';
 
 function App() {
+  console.log(seahorse);
   return (
     <div className="App">
       <header className="App-header">      
         <p>Seahorse</p>
       </header>
       <section>
-        <model-viewer alt="A 3D model of an seahorse." src={seahorse} magic-leap ar />
+        <model-viewer alt="A 3D model of an seahorse." src={seahorse} ar ></model-viewer>
       </section>
     </div>
   );

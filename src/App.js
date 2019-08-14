dcbf4415bd2e1fcd49cce4f192edd378ed790b86
Brad Tracy <btracy@everesttech.com>
2019-08-14 09:04:50 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import '@google/model-viewer';
 import './App.css';
+import * as seahorse from './assets/seahorse.gltf';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header">      
+        <p>Seahorse</p>
       </header>
+      <section>
+        <model-viewer alt="A 3D model of an seahorse." src={seahorse} magic-leap ar />
+      </section>
     </div>
   );
 }

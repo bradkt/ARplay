c07af0eb31695e1d71910cffbddd457fcf98eaa7
Brad Tracy <btracy@everesttech.com>
2019-08-15 15:11:00 -0400

    back to seahorse

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import '@google/model-viewer';
 import './App.css';
-import * as seahorse from './assets/b1.gltf';
+import * as seahorse from './assets/seahorse.gltf';
 
-function App() {
-  console.log(seahorse);
-  return (
-    <div className="App">
-      <header className="App-header">      
-        <p>Seahorse</p>
-      </header>
-      <section>
-        <model-viewer alt="A 3D model of an seahorse." src={seahorse} ar ></model-viewer>
-      </section>
-    </div>
-  );
+class App extends React.Component {
+
+  componentDidMount() {
+
+    
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">      
+          <p>Sea Horse</p>
+        </header>
+        <section>
+          <model-viewer alt="A 3D model of an seahorse." src={seahorse} ar ></model-viewer>
+        </section>
+      </div>
+    );
+  }
 }
 
 export default App;
